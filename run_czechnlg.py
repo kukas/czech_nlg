c18bb7b27d09bf355ecaf12502b68bbaa4287a55
Jirka Balhar <balhar.j@gmail.com>
2022-06-10 11:15:02 +0200

    more evaluation

--- run_czechnlg.py
+++ run_czechnlg.py
@@ -26,10 +26,14 @@ from transformers import (
 from transformers.trainer_utils import get_last_checkpoint
 
 import numpy as np
+import pandas as pd
 
 import datasets
 from datasets import load_dataset
+
 import evaluate
+
+# from evaluation import run_e2e_metrics, run_slot_error_rate_metric
 
 logger = logging.getLogger(__name__)
 
@@ -302,17 +306,26 @@ class DatasetPreprocessing:
 
 class Evaluation:
     def __init__(self, tokenizer, ignore_pad_token_for_loss):
+        self.trainer = None
         self.tokenizer = tokenizer
         self.ignore_pad_token_for_loss = ignore_pad_token_for_loss
+        self.metric_key_prefix = "eval"
 
         # Metric
-        self.metric = evaluate.load("sacrebleu")
+        self.bleu = evaluate.load("sacrebleu")
+        self.chrf = evaluate.load("chrf")
+        # self.rouge = evaluate.load("rouge")
+        self.meteor = evaluate.load("meteor")
+        self.ter = evaluate.load("ter")
+        self.bertscore = evaluate.load("bertscore")
+        # self.bleurt = evaluate.load("bleurt", config_name="BLEURT-20")
 
-    def postprocess_text(self, preds, labels):
+    def postprocess_text(self, preds, _labels):
         preds = [pred.strip() for pred in preds]
-        labels = [[label.strip()] for label in labels]
+        labels = [[label.strip()] for label in _labels]
+        flat_labels = [label.strip() for label in _labels]
 
-        return preds, labels
+        return preds, labels, flat_labels
 
     def compute_metrics(self, eval_preds):
         preds, labels = eval_preds
@@ -326,20 +339,80 @@ class Evaluation:
         decoded_labels = self.tokenizer.batch_decode(labels, skip_special_tokens=True)
 
         # Some simple post-processing
-        decoded_preds, decoded_labels = self.postprocess_text(
-            decoded_preds, decoded_labels
-        )
+        sys, refs, flat_refs = self.postprocess_text(decoded_preds, decoded_labels)
 
-        result = self.metric.compute(
-            predictions=decoded_preds, references=decoded_labels
+        result = {}
+
+        # da = list(self.trainer.eval_dataset["input"])
+        # print(da)
+        # assert len(da) == len(sys)
+        # e2e_result = run_e2e_metrics(sys, refs)
+        # for metric_name, score in e2e_result:
+        #     result[f"E2E_{metric_name}"] = score
+        # ser = run_slot_error_rate_metric(sys, da)
+        # result["SER"] = ser
+
+        # n-gram based
+        result["BLEU"] = self.bleu.compute(predictions=sys, references=refs)["score"]
+        result["BLEU_lowercase"] = self.bleu.compute(
+            predictions=sys, references=refs, lowercase=True
+        )["score"]
+        result["chrF++"] = self.chrf.compute(
+            predictions=sys,
+            references=refs,
+            word_order=2,
+        )["score"]
+        result["TER"] = self.ter.compute(predictions=sys, references=refs)["score"]
+
+        # result["rougeL"] = self.rouge.compute(
+        #     predictions=sys, references=flat_refs
+        # )["rougeL"]
+        meteor = self.meteor.compute(predictions=sys, references=flat_refs)["meteor"]
+        result["meteor"] = meteor
+
+        # embedding based
+        bertscore = self.bertscore.compute(
+            predictions=sys, references=flat_refs, lang="cs"
         )
-        result = {"bleu": result["score"]}
+        result["BERTscore_precision"] = np.mean(bertscore["precision"])
+        result["BERTscore_recall"] = np.mean(bertscore["recall"])
+        result["BERTscore_f1"] = np.mean(bertscore["f1"])
+
+        # bleurt = self.bleurt.compute(predictions=sys, references=flat_refs)
+        # result["BLEURT"] = np.mean(bleurt["score"])
 
         prediction_lens = [
             np.count_nonzero(pred != self.tokenizer.pad_token_id) for pred in preds
         ]
         result["gen_len"] = np.mean(prediction_lens)
+
         result = {k: round(v, 4) for k, v in result.items()}
+
+        assert self.trainer is not None
+        step = self.trainer.state.global_step
+        output_dir = self.trainer.args.output_dir
+        df = pd.DataFrame(
+            {
+                "prediction": sys,
+                "reference": flat_refs,
+                "bertscore_precision": bertscore["precision"],
+                "bertscore_recall": bertscore["recall"],
+                "bertscore_f1": bertscore["f1"],
+            }
+        )
+        csv_path = os.path.join(
+            output_dir, f"outputs_{self.metric_key_prefix}-{step}.csv"
+        )
+        df.to_csv(csv_path)
+        logger.info(f"Evaluation set outputs saved in {csv_path}")
+
+        df = pd.DataFrame([result])
+        csv_path = os.path.join(
+            output_dir, f"results_{self.metric_key_prefix}-{step}.csv"
+        )
+        df.to_csv(csv_path)
+        logger.info(f"Evaluation set results saved in {csv_path}")
+
         return result
 
 
@@ -506,6 +579,9 @@ def main():
         data_collator=data_collator,
         compute_metrics=compute_metrics,
     )
+
+    # TODO: rewrite?
+    evaluation.trainer = trainer
 
     # Training
     # - custom evaluation function
--- run_czechnlg.sh
+++ run_czechnlg.sh
@@ -2,39 +2,56 @@ export HF_DATASETS_CACHE="/scratch/project/open-24-11/balharj/cache/datasets"
 export TRANSFORMERS_CACHE="/scratch/project/open-24-11/balharj/cache"
 # export WANDB_DISABLED="true"
 
-    # --max_train_samples 10 \
-    # --max_eval_samples 10 \
-    # --max_predict_samples 10 \
+# python run_czechnlg.py \
+#     --do_train \
+#     --do_eval \
+#     --model_name_or_path Helsinki-NLP/opus-mt-en-cs \
+#     --dataset_name cs_restaurants \
+#     --output_dir /scratch/project/open-24-11/balharj/experiments_debug/opus-mt-en-cs-finetune-cs_restaurants \
+#     --per_device_train_batch_size=8 \
+#     --per_device_eval_batch_size=8 \
+#     --max_steps=20000 \
+#     --learning_rate 5e-6 \
+#     --save_steps 100 \
+#     --eval_steps 100 \
+#     --evaluation_strategy steps \
+#     --overwrite_output_dir \
+#     --predict_with_generate \
+#     --report_to none
+
+# Testing
 python run_czechnlg.py \
     --do_train \
+    --do_eval \
     --model_name_or_path Helsinki-NLP/opus-mt-en-cs \
     --dataset_name cs_restaurants \
-    --output_dir /scratch/project/open-24-11/balharj/experiments_debug/opus-mt-en-cs-finetune-cs_restaurants \
-    --per_device_train_batch_size=8 \
-    --per_device_eval_batch_size=8 \
-    --max_steps=20000 \
-    --learning_rate 5e-6 \
-    --save_steps 100 \
-    --eval_steps 100 \
-    --evaluation_strategy steps \
+    --output_dir /scratch/project/open-24-11/balharj/experiments_debug/opus-mt-en-cs-finetune-cs_restaurants-10-samples \
+    --max_train_samples 100 \
+    --max_eval_samples 10 \
+    --max_predict_samples 10 \
+    --num_train_epochs 20 \
+    --evaluation_strategy epoch \
+    --save_strategy epoch \
     --overwrite_output_dir \
     --predict_with_generate \
     --report_to none
 
-# python run_translation.py \
-#     --do_train \
-#     --model_name_or_path Helsinki-NLP/opus-mt-en-cs \
-#     --dataset_name cs_restaurants \
-#     --source_lang en_XX \
-#     --target_lang cs_CZ \
-#     --output_dir /scratch/project/open-24-11/balharj/experiments_debug/opus-mt-en-cs-finetune-cs_restaurants-50-samples \
-#     --max_train_samples 10 \
-#     --max_eval_samples 10 \
-#     --max_predict_samples 10 \
-#     --evaluation_strategy epoch \
-#     --overwrite_output_dir \
-#     --predict_with_generate \
-#     --report_to none
+# Testing mBART
+python run_czechnlg.py \
+    --do_train \
+    --do_eval \
+    --model_name_or_path facebook/mbart-large-50-one-to-many-mmt \
+    --dataset_name cs_restaurants \
+    --output_dir /scratch/project/open-24-11/balharj/experiments_debug/opus-mt-en-cs-finetune-cs_restaurants-10-samples \
+    --max_train_samples 100 \
+    --max_eval_samples 10 \
+    --max_predict_samples 10 \
+    --num_train_epochs 20 \
+    --evaluation_strategy epoch \
+    --save_strategy epoch \
+    --overwrite_output_dir \
+    --predict_with_generate \
+    --report_to none
 
 # python run_czechnlg.py \
 #     --model_name_or_path facebook/mbart-large-50-one-to-many-mmt \
