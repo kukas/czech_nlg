03a3d59d6c8b80c0348f6962e66ff07bc5f17bb9
Jirka Balhar <balhar.j@gmail.com>
2022-10-04 16:48:51 +0200

    clean up old scripts

--- run_experiments.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-jobID=`qsub -j y -cwd -q gpu.q -l gpu=1 run.sh train_text2text.py --batch-size 16 --epochs 10 --checkpoint google/mt5-small`
-
-#jobID=`echo $jobID | awk 'match($0,/[0-9]+/){print substr($0, RSTART, RLENGTH)}'`
-
-#sleep 10s
-#tail -f run.sh.o$jobID
